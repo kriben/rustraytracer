26fa1a772bed82fa70253ed47c0a723fb7a07ad6
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2015-04-27 10:02:54 +0200

    Rename Vec to Vec3 to avoid clash with std::Vec.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::ops::Add;
 use std::ops::Sub;
 use std::ops::Mul;
 pub mod vec3;
-use vec3::Vec;
+use vec3::Vec3;
 extern crate rand;
 
 #[test]
 fn ray_has_origin_and_destination() {
-    let origin = Vec::new(1.0, 2.0, 3.0);
-    let direction = Vec::new(4.0, 5.0, 6.0);
+    let origin = Vec3::new(1.0, 2.0, 3.0);
+    let direction = Vec3::new(4.0, 5.0, 6.0);
     let ray = Ray::new(origin, direction);
     assert_eq!(ray.origin.x, origin.x);
     assert_eq!(ray.direction.x, direction.x);
@@ -17,12 +17,12 @@ fn ray_has_origin_and_destination() {
 
 
 struct Ray {
-    origin: vec3::Vec,
-    direction: Vec
+    origin: vec3::Vec3,
+    direction: Vec3
 }
 
 impl Ray {
-    fn new(origin: Vec, direction: Vec) -> Ray {
+    fn new(origin: Vec3, direction: Vec3) -> Ray {
         Ray { origin: origin, direction: direction }
     }
 }
@@ -37,8 +37,8 @@ fn tent_filter() -> f64 {
     }
 }
 
-fn radiance(r: Ray, depth: i32) -> Vec {
-    Vec::new(0.0, 0.0, 0.0)
+fn radiance(r: Ray, depth: i32) -> Vec3 {
+    Vec3::new(0.0, 0.0, 0.0)
 }
 
 #[cfg(not(test))]
@@ -48,28 +48,28 @@ fn main() {
     let samps = 500/4;
 
     // Camera position
-    let cam = Ray::new(Vec::new(50.0, 52.0, 295.6),
-                       Vec::normalized(Vec::new(0.0, -0.042612, -1.0)));
+    let cam = Ray::new(Vec3::new(50.0, 52.0, 295.6),
+                       Vec3::normalized(Vec3::new(0.0, -0.042612, -1.0)));
     // x direction increment
-    let cx = Vec::new(w as f64 * 0.5135 / h as f64, 0.0, 0.0);
+    let cx = Vec3::new(w as f64 * 0.5135 / h as f64, 0.0, 0.0);
     // y diraction increment
-    let cy = Vec::normalized(Vec::cross(cx, cam.direction)) * 0.5135;
+    let cy = Vec3::normalized(Vec3::cross(cx, cam.direction)) * 0.5135;
     
 
     for y in 0..h { // Loop over image rows
         for x in 0..w { // Loop over image cols
             for sy in 0..2 { // 2x2 subpixel rows
                 for sx in 0..2 { // 2x2 subpixel cols
-                    let mut r = Vec::new(0.0, 0.0, 0.0);
+                    let mut r = Vec3::new(0.0, 0.0, 0.0);
                     for _ in 0..samps {
                         let dx = tent_filter();
                         let dy = tent_filter();
-                        let d : Vec =
+                        let d : Vec3 =
                             cx * (((sx as f64 + 0.5 + dx) / 2.0 + x as f64) / w as f64 - 0.5) +
                             cy * (((sy as f64 + 0.5 + dy) / 2.0 + y as f64) / h as f64 - 0.5) + 
                             cam.direction;
                         let ray = Ray::new(cam.origin + d * 140.0,
-                                           Vec::normalized(d));
+                                           Vec3::normalized(d));
                         r = r + radiance(ray, 0) * (1.0 / samps as f64);
                     }
                 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -7,7 +7,7 @@ fn vec_constructor_works() {
     let x = 1.0f64;
     let y = 2.0f64;
     let z = 3.0f64;
-    let vec = Vec::new(x, y, z);
+    let vec = Vec3::new(x, y, z);
     assert_eq!(x, vec.x);
     assert_eq!(y, vec.y);
     assert_eq!(z, vec.z);
@@ -15,7 +15,7 @@ fn vec_constructor_works() {
 
 #[test]
 fn vec_norm_works() {
-    let mut vec = Vec::new(1.0, 1.0, 1.0);
+    let mut vec = Vec3::new(1.0, 1.0, 1.0);
     vec.norm();
     let expected = 1.0 / (3.0f64).sqrt();
     assert_eq!(vec.x, expected);
@@ -25,23 +25,23 @@ fn vec_norm_works() {
 
 #[test]
 fn vec_dot_product_same_direction_is_one() {
-    let direction = Vec::new(0.0, 1.0, 0.0);
+    let direction = Vec3::new(0.0, 1.0, 0.0);
     assert_eq!(direction.dot(direction), 1.0);
 }
 
 #[test]
 fn vec_dot_product_opposite_direction_is_minus_one() {
-    let up = Vec::new(0.0, 1.0, 0.0);
-    let down = Vec::new(0.0, -1.0, 0.0);
+    let up = Vec3::new(0.0, 1.0, 0.0);
+    let down = Vec3::new(0.0, -1.0, 0.0);
     assert_eq!(up.dot(down), -1.0);
     assert_eq!(down.dot(up), -1.0);
 }
 
 #[test]
 fn vec_cross_product_test() {
-    let up = Vec::new(0.0, 1.0, 0.0);
-    let right = Vec::new(1.0, 0.0, 0.0);
-    let crossed = Vec::cross(up, right);
+    let up = Vec3::new(0.0, 1.0, 0.0);
+    let right = Vec3::new(1.0, 0.0, 0.0);
+    let crossed = Vec3::cross(up, right);
     assert_eq!(crossed.x, 0.0);
     assert_eq!(crossed.y, 0.0);
     assert_eq!(crossed.z, -1.0);
@@ -49,8 +49,8 @@ fn vec_cross_product_test() {
 
 #[test]
 fn vec_addition() {
-    let x = Vec::new(1.0, 2.0, 3.0);
-    let y = Vec::new(7.0, 8.0, 9.0);
+    let x = Vec3::new(1.0, 2.0, 3.0);
+    let y = Vec3::new(7.0, 8.0, 9.0);
     let res = x + y;
     assert_eq!(res.x, 8.0);
     assert_eq!(res.y, 10.0);
@@ -59,8 +59,8 @@ fn vec_addition() {
 
 #[test]
 fn vec_subtraction() {
-    let x = Vec::new(1.0, 2.0, 3.0);
-    let y = Vec::new(7.0, 8.0, 9.0);
+    let x = Vec3::new(1.0, 2.0, 3.0);
+    let y = Vec3::new(7.0, 8.0, 9.0);
     let res = x - y;
     assert_eq!(res.x, -6.0);
     assert_eq!(res.y, -6.0);
@@ -69,7 +69,7 @@ fn vec_subtraction() {
 
 #[test]
 fn vec_multiplication_f64() {
-    let x = Vec::new(1.0, 2.0, 3.0);
+    let x = Vec3::new(1.0, 2.0, 3.0);
     let res = x * 2.0;
     assert_eq!(res.x, 2.0);
     assert_eq!(res.y, 4.0);
@@ -77,15 +77,15 @@ fn vec_multiplication_f64() {
 }
 
 #[derive(Clone, Copy)]
-pub struct Vec {
-    x: f64,
-    y: f64,
-    z: f64
+pub struct Vec3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64
 }
 
-impl Vec {
-    pub fn new(x: f64, y: f64, z: f64) -> Vec {
-        Vec { x: x, y: y, z: z }
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
+        Vec3 { x: x, y: y, z: z }
     }
 
     pub fn norm(&mut self) {
@@ -95,49 +95,49 @@ impl Vec {
         self.z /= length;
     }
 
-    pub fn normalized(v: Vec) -> Vec {
+    pub fn normalized(v: Vec3) -> Vec3 {
         let length = v.dot(v).sqrt();
-        return Vec::new(v.x / length,
+        return Vec3::new(v.x / length,
                         v.y / length,
                         v.z / length);
     }
 
-    pub fn dot(&self, other: Vec) -> f64 {
+    pub fn dot(&self, other: Vec3) -> f64 {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
-    pub fn cross(u: Vec, v: Vec) -> Vec {
-        return Vec::new(u.y * v.z - u.z * v.y,
+    pub fn cross(u: Vec3, v: Vec3) -> Vec3 {
+        return Vec3::new(u.y * v.z - u.z * v.y,
                         u.z * v.x - u.x * v.z,
                         u.x * v.y - u.y * v.x)
     }
 }
 
-impl Add for Vec {
-    type Output = Vec;
+impl Add for Vec3 {
+    type Output = Vec3;
 
-    fn add(self, _rhs: Vec) -> Vec {
-        Vec::new(self.x + _rhs.x,
+    fn add(self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(self.x + _rhs.x,
                  self.y + _rhs.y,
                  self.z + _rhs.z)
     }
 }
 
-impl Sub for Vec {
-    type Output = Vec;
+impl Sub for Vec3 {
+    type Output = Vec3;
 
-    fn sub(self, _rhs: Vec) -> Vec {
-        Vec::new(self.x - _rhs.x,
+    fn sub(self, _rhs: Vec3) -> Vec3 {
+        Vec3::new(self.x - _rhs.x,
                  self.y - _rhs.y,
                  self.z - _rhs.z)
     }
 }
 
-impl Mul<f64> for Vec {
-    type Output = Vec;
+impl Mul<f64> for Vec3 {
+    type Output = Vec3;
 
-    fn mul(self, rhs: f64) -> Vec {
-        return Vec::new(self.x * rhs, self.y * rhs, self.z * rhs);
+    fn mul(self, rhs: f64) -> Vec3 {
+        return Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs);
     }
 }
 
